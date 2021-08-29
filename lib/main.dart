a0e0d64b4c7aced14c5ac282366be8b560b9660d
unknown <duynd0504@gmail.com>
2021-08-29 07:59:16 +0700

    done data

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_ecomerience_21/packages/quote/quote.dart';
+import 'package:flutter_ecomerience_21/packages/quote/quote_model.dart';
 import 'package:flutter_ecomerience_21/pages/landing_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Quotes().getAll();
   runApp(MyApp());
 }
 
---
+++ lib/models/englishtoday.dart
@@ -0,0 +1,9 @@
+class EnglishToday {
+  String? id;
+  String? noun;
+  String? quote;
+  // khai bao bien bool
+  bool isFavorite;
+
+  EnglishToday({this.id, this.noun, this.quote, this.isFavorite = false});
+}
